2367c69523e0b5cd90e5eb2f9e86d35df20dd738
fedepaloz <palozzifederico@gmail.com>
2022-07-05 14:32:18 +0200

    milestone 0

---
+++ script.js
@@ -0,0 +1,49 @@
+const userInfos = [
+    {
+        name: 'wayne barnett',
+        job: 'founder & ceo',
+        idPIc: 'wayne-barnett-founder-ceo.jpg'
+
+
+    }
+
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor ',
+        idPIc: 'angela-caroll-chief-editor.jpg'
+
+
+    }
+    
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        idPIc: 'walter-gordon-office-manager.jpg'
+
+
+    }
+    
+    {
+        name: 'angela lopez',
+        job: 'social media manager',
+        idPIc: 'angela-lopez-social-media-manager.jpg'
+
+
+    }
+    
+    {
+        name: 'scott estrada',
+        job: 'developer',
+        idPIc: 'scott-estrada-developer.jpg'
+
+
+    }
+    
+    {
+        name: 'barbara ramos',
+        job: 'graphic designer',
+        idPIc: 'barbara-ramos-graphic-designer.jpg '
+
+
+    }
+]
